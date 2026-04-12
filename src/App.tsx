6c688a630546a67f32244eb9c8686a5a950f2ffe
William Huggins <messagewillh@gmail.com>
2026-04-12 09:45:14 -0700

    Initialize codex/add-responsive-image-support-site-wide

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import MobileBar from './pages/MobileBar';
+import Careers from './pages/Careers';
+import GiftCards from './pages/GiftCards';
+import Subscribe from './pages/Subscribe';
+import ComingSoon from './pages/ComingSoon';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="about" element={<About />} />
+          <Route path="mobile-bar" element={<MobileBar />} />
+          <Route path="careers" element={<Careers />} />
+          <Route path="gift-cards" element={<GiftCards />} />
+          <Route path="subscribe" element={<Subscribe />} />
+          <Route path="privacy" element={<ComingSoon />} />
+          <Route path="terms" element={<ComingSoon />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
